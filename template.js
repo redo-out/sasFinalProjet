0bcd5f4fd0aafd98ddea8d407f4b88a5fa864f01
redo-out <lordshadow0704@gmail.com>
2026-09-10 14:52:17 +0100

    fixed buyticket and added some more

--- template.js
+++ template.js
@@ -3,9 +3,8 @@ import { trips } from "./arrayOfObjet.js";
 let prompt = PromptSync();
 import PromptSync from 'prompt-sync';
 
-let ID = 1;
+let ID = 0;
 let tickets = [];
-let found = false;
 let n;
 
 console.log("====== Railway Manager ======", '\n');
@@ -19,7 +18,7 @@ console.log("7. Trier les trajets");
 console.log("0. Quitter", '\n');
 
 do {
-    
+
     n = +prompt('Entrer une choix : ');
     switch (n) {
         case 1: {
@@ -33,11 +32,12 @@ do {
             break;
         }
         case 3: {
+            desplayTicket();
 
             break;
         }
         case 4: {
-
+            console.log(annulerTicket());
             break;
         }
         case 5: {
@@ -91,27 +91,83 @@ function verifierSeats(seatNum) {
 function creatTicket(tab, passagerNom, trajetId) {
 
     for (let indx = 0; indx < tab.length; indx++) {
-           if (trajetId == tab[indx].id && verifierSeats(tab[indx].availableSeats) == true ) {
+        if (trajetId == tab[indx].id && verifierSeats(tab[indx].availableSeats) == true) {
             let ticket = {
                 id: ID,
                 passangername: passagerNom,
                 tripId: trajetId,
+                depart: tab[indx].departure,
+                destination: tab[indx].destination,
                 Seatnumber: 51 - tab[indx].availableSeats,
                 price: tab[indx].price
             }
-            found = true;
             tickets[tickets.length] = ticket;
-            tab[indx].availableSeats -= 1;
-            console.log(tab[indx].availableSeats);
-            console.log(`Have a safe trip :) `);
+            trips[indx].availableSeats--;
             ID++;
+            return `
+           Ticket purchased successfully.
+
+Ticket #${ID}
+Passenger: ${passagerNom}
+Trip: ${tab[indx].departure} → ${tab[indx].destination}
+Seat: ${ticket.Seatnumber}
+Price: ${tab[indx].price} DH `;
         }
     }
-    if (!found) { console.log("No valid ticket created."); }
-    return (tickets[ID - 2]);
+    return "couldn't creat your ticket";
 }
 
+function desplayTicket() {
+
+    if (tickets.length == 0) {
+        console.log("there's no tickets");
+        return;
+    }
+    let showTicket = "       TICKETS       \n ";
+    for (let i = 0; i < tickets.length; i++) {
+        let ticket = tickets[i];
+        showTicket += "\n";
+        showTicket += "Ticket #" + (ticket.id + 1) + "\n";
+        showTicket += "passager : " + ticket.passangername + "\n";
+        showTicket += "trajet : " + ticket.depart + " → " + ticket.destination + "\n";
+        showTicket += "Place : " + ticket.Seatnumber + "\n";
+        showTicket += "Prix : " + ticket.price + " DH \n";
+    }
+    console.log(showTicket);
+}
+
+function annulerTicket(){
+    if (tickets.length <= 0){
+        return "aucune ticket";
+    }
+    let identif = +prompt("Entre ID: ");
+    let tripId = 0;
+    let found = false;
+    for (let indx = 0; indx < tickets.length; indx++){
+        if (identif == tickets[indx].ID){
+        tripId == tickets[indx].trajetId;
+        tickets.splice(indx, 1);
+        trips[tripId].availableSeats += 1;
+        found = true;
+        break;
+        }
+    }
+    return `Ticket ${identif} annulé avec succès.`;
+}
+
+function rechercherTicket(){
+    let name = prompt("entre passager nom : ")
+    let ticket = [];
+    for (let indx = 0; indx < tickets.length; i++){
+        if (name == tickets[indx].passangername){
+            ticket += tickets[indx].passangername; 
+        }
+    }
+    return ticket;
+}
+
+function filtrerTrajets(dest){
+    let results = [];
 
 
-
-
+}
