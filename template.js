94acac72576702bd7fe40b65290fcd69e7a6adf3
redo-out <lordshadow0704@gmail.com>
2026-09-10 23:30:08 +0100

    found some holes on rechercher and annuler

--- template.js
+++ template.js
@@ -27,7 +27,7 @@ do {
         case 2: {
             const passagerNom = prompt('Nom du passager : ');
             const trajetId = +prompt('Identifiant du trajet : ');
-            console.log(creatTicket(trips, passagerNom, trajetId))
+            console.log(creatTicket(trips, passagerNom, trajetId));
             break;
         }
         case 3: {
@@ -40,7 +40,7 @@ do {
             break;
         }
         case 5: {
-            rechercherTicket();
+            console.log(rechercherTicket());
             break;
         }
         case 6: {
@@ -72,11 +72,14 @@ function afficherTragets(tab) {
 }
 
 function verifierId(numId) {
-    if (numId == trips.id)
-        return true;
-    else
-        return false;
+    for (let i = 0; i < trips.length; i++) {
+        if (numId == trips.id) {
+            return true;
+        }
+    }
+    return false;
 }
+
 
 function verifierSeats(seatNum) {
     if (seatNum > 0) {
@@ -88,6 +91,7 @@ function verifierSeats(seatNum) {
 }
 
 function creatTicket(tab, passagerNom, trajetId) {
+
 
     for (let indx = 0; indx < tab.length; indx++) {
         if (trajetId == tab[indx].id && verifierSeats(tab[indx].availableSeats) == true) {
@@ -103,8 +107,8 @@ function creatTicket(tab, passagerNom, trajetId) {
             tickets[tickets.length] = ticket;
             trips[indx].availableSeats--;
             return `
-           Ticket purchased successfully.
-
+            Ticket purchased successfully.
+            
 Ticket #${ticket.id}
 Passenger: ${passagerNom}
 Trip: ${tab[indx].departure} → ${tab[indx].destination}
@@ -112,8 +116,11 @@ Seat: ${ticket.Seatnumber}
 Price: ${tab[indx].price} DH \n`;
         }
     }
-    return "couldn't creat your ticket";
+    if (!verifierId(trajetId)) {
+        return "couldn't creat your ticket"
+    }
 }
+
 
 function desplayTicket() {
 
@@ -134,45 +141,53 @@ function desplayTicket() {
     console.log(showTicket);
 }
 
-function annulerTicket(){
+function annulerTicket() {
     let identif = +prompt("Entre ID: ");
-    if (tickets.length <= 0){
+    if (tickets.length <= 0) {
         return "aucune ticket";
     }
-    let found = [];
-    for (let indx = 0; indx < tickets.length; indx++){
-        if (identif == tickets[indx].id){
-        found = tickets[indx];
-        tickets.splice(indx, 1);
-        break;
+    let found = "";
+    for (let indx = 0; indx < tickets.length; indx++) {
+        if (identif === tickets[indx].id) {
+            found = tickets[indx];
+            for (let i = 0; i < trips.length; i++) {
+                if (trips[i].id === found.tripId) {
+                    trips[i].availableSeats++;
+                    break;
+                }
+            }
+            tickets.splice(indx, 1);
+            break;
         }
-        if (!found){
-            return "ticket not found";      
-          }
-        }
-        found.availableSeats++;
-        return `Ticket ${identif} annulé avec succès.`;
     }
-
-function rechercherTicket(){
-    let name = prompt("entre passager nom : ")
-    let ticket = [];
-    if (ticket.length == 0){
-        console.log("couldn't find ur ticket");
+    if (!found) {
+        return "ticket not found";
     }
-    for (let indx = 0; indx < tickets.length; indx++){
-        if (name == tickets[indx].passangername){
-            ticket[ticket.length] = tickets[indx]; 
-        }
-        console.log(`
-    Ticket #${ticket[indx].id}
-    Passenger: ${name}
-    Trip: ${ticket[indx].depart} → ${ticket[indx].destination}
-    Seat: ${ticket[indx].Seatnumber}
-    Price: ${ticket[indx].price} DH`);
-    }
+    return `Ticket ${identif} annulé avec succès.`;
 }
-function filtrerTrajets(dest){
+
+function rechercherTicket() {
+    let name = prompt("entre passager nom : ")
+    let result = "";
+    for (let i = 0; i < tickets.length; i++) {
+        if (name == tickets[i].passangername) {
+            // ticket.push(tickets[i]);
+            result += `Ticket #${tickets[i].id}
+Passenger: ${name}
+Trip: ${tickets[i].depart} → ${tickets[i].destination}
+Seat: ${tickets[i].Seatnumber}
+Price: ${tickets[i].price} DH
+==========================\n`;
+        }
+        }
+        if (result == "") {
+            return "couldn't find ur ticket";
+    }
+    return result;
+    }
+//     for (let j = 0; j < tickets.length; j++){
+// }
+function filtrerTrajets(dest) {
     let results = [];
 
 
